94aa661e6a9f2e2f4345ef5370981ad5d5d20cb5
Saloni Shikha <shikhasalonee@gmail.com>
2019-06-11 16:54:45 +0545

    heroku test

--- LMS/urls.py
+++ LMS/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rest-auth/', include('rest_auth.urls')),
     path('', include('WebApp.urls')),
-    path('forum/', include('forum.urls')),
+    path('forum/',include('forum.urls')),
+
 ]
