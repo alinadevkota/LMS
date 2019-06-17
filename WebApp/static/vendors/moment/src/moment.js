b0826d9c7f6ea761e2d69ca62a5555dbd513a09a
Sushant Gautam <susant.gautam@gmail.com>
2019-06-17 19:37:07 +0545

    added static files

--- WebApp/static/vendors/moment/src/moment.js
+++ WebApp/static/vendors/moment/src/moment.js
@@ -4,45 +4,44 @@
 //! license : MIT
 //! momentjs.com
 
-import { hooks as moment, setHookCallback } from './lib/utils/hooks';
-
-moment.version = '2.13.0';
-
+import {hooks as moment, setHookCallback} from './lib/utils/hooks';
 import {
-    min,
-    max,
-    now,
+    createInvalid as invalid,
+    createInZone as parseZone,
+    createLocal as local,
+    createUnix as unix,
+    createUTC as utc,
     isMoment,
+    max,
+    min,
     momentPrototype as fn,
-    createUTC       as utc,
-    createUnix      as unix,
-    createLocal     as local,
-    createInvalid   as invalid,
-    createInZone    as parseZone
+    now
 } from './lib/moment/moment';
 
 import {
     defineLocale,
-    updateLocale,
+    getLocale as localeData,
     getSetGlobalLocale as locale,
-    getLocale          as localeData,
-    listLocales        as locales,
-    listMonths         as months,
-    listMonthsShort    as monthsShort,
-    listWeekdays       as weekdays,
-    listWeekdaysMin    as weekdaysMin,
-    listWeekdaysShort  as weekdaysShort
+    listLocales as locales,
+    listMonths as months,
+    listMonthsShort as monthsShort,
+    listWeekdays as weekdays,
+    listWeekdaysMin as weekdaysMin,
+    listWeekdaysShort as weekdaysShort,
+    updateLocale
 } from './lib/locale/locale';
 
 import {
-    isDuration,
     createDuration as duration,
-    getSetRelativeTimeThreshold as relativeTimeThreshold
+    getSetRelativeTimeThreshold as relativeTimeThreshold,
+    isDuration
 } from './lib/duration/duration';
 
-import { normalizeUnits } from './lib/units/units';
+import {normalizeUnits} from './lib/units/units';
 
 import isDate from './lib/utils/is-date';
+
+moment.version = '2.13.0';
 
 setHookCallback(local);
 
