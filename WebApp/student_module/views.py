06a7887a0a69ad819dda960a886c18ceb7916f53
Sushant Gautam <susant.gautam@gmail.com>
2019-06-30 13:29:58 +0545

    teacher and student module added

---
+++ WebApp/student_module/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def start(request):
+    """Start page with a documentation.
+    """
+    # return render(request,"start.html")
+    return render(request, "student_module\homepage.html")
