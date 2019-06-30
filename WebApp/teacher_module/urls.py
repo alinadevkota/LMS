972c2425b1a19fa65419e298290304c861e6254d
Sushant Gautam <susant.gautam@gmail.com>
2019-06-30 13:29:58 +0545

    different base for different module

---
+++ WebApp/teacher_module/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.urls import path
+
+from WebApp.teacher_module import views
+
+urlpatterns = (
+    # urls for TodoTInfo
+    path('', login_required(views.start), name='teacher_home'),
+)
+
+
+
