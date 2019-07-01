0b88b905a7aeeb99129c687c687ac9acf1937cb8
Sushant Gautam <susant.gautam@gmail.com>
2019-07-01 06:38:31 +0545

    url fix

--- WebApp/student_module/views.py
+++ WebApp/student_module/views.py
@@ -5,4 +5,4 @@ def start(request):
     """Start page with a documentation.
     """
     # return render(request,"start.html")
-    return render(request, "student_module\homepage.html")
+    return render(request, "student_module/homepage.html")
--- WebApp/teacher_module/views.py
+++ WebApp/teacher_module/views.py
@@ -5,4 +5,4 @@ def start(request):
     """Start page with a documentation.
     """
     # return render(request,"start.html")
-    return render(request, "teacher_module\homepage.html")
+    return render(request, "teacher_module/homepage.html")
