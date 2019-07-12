d6653ef8252d40d64778cc7b3c4266a206ecc496
Bidhan Rai <bidhanrai424@gmail.com>
2019-07-12 16:47:49 +0545

    course list page added

--- WebApp/student_module/urls.py
+++ WebApp/student_module/urls.py
@@ -19,7 +19,8 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name="students_dashboard"),
     path('mycourse/', views.mycourse, name="students_mycourse"),
     path('quiz/', views.quiz, name="students_quiz"),
-    path('quizzes/', views.quizzes),
+    path('quizzes/', views.quizzes, name="quiz_question"),
     path('calendar/', views.calendar, name="students_calendar"),
+    path('course_detail/', views.coursedetail, name="course_detail"),
     path('coursesList/', views.coursesList, name="students_coursesList")
 ]
