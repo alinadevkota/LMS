fdc5ac39b635959eceab85ff5120a9c0cf0444a2
spandan12 <spandan.pyakurel.1996@gmail.com>
2019-07-16 14:15:57 +0545

    automates the process of deleting migrations, re-migrations of quiz, forum and WebApp and creates superuser(id-nsdevil, pw-nsdevil

---
+++ format.bat
@@ -0,0 +1,7 @@
+rmdir /s /q WebApp\migrations
+rmdir /s /q quiz\migrations
+rmdir /s /q forum\migrations
+del db.sqlite3
+python manage.py makemigrations forum WebApp quiz
+python manage.py migrate
+echo "python manage.py createsuperuserwithpassword    --username nsdevil --password nsdevil --email admin@example.org    --preserve" | python manage.py shell
