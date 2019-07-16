de2714a4cdff49f3b3cb4d3faa7c81b7412acb18
Bidhan Rai <bidhanrai424@gmail.com>
2019-07-16 13:14:29 +0545

    Student Mycourse static page and teacher lecture list

--- WebApp/student_module/urls.py
+++ WebApp/student_module/urls.py
@@ -22,7 +22,15 @@ urlpatterns = [
     path('quiz/', views.quiz, name="students_quiz"),
     path('quizzes/', views.quizzes, name="quiz_question"),
     path('calendar/', views.calendar, name="students_calendar"),
-    # path('course_detail/', views.coursedetail, name="course_detail"),
+    path('course_detail/', views.coursedetail, name="course_detail"),
     path('coursesList/', views.coursesList, name="students_coursesList"),
     url(r'^courseList/(?P<pk>\d+)/$', views.coursedetail, name='course_detail'),
 ]
+
+urlpatterns += (
+    # urls for ChapterInfo
+    #path('chapterinfo/', views.ChapterInfoListView.as_view(), name='chapterinfo_list'),
+    #path('chapterinfo/create/', views.ChapterInfoCreateView.as_view(), name='chapterinfo_create'),
+    path('lectureinfo/detail/<int:pk>/', views.LectureInfoDetailView.as_view(), name='studentcourse_detail'),
+   # path('chapterinfo/update/<int:pk>/', views.ChapterInfoUpdateView.as_view(), name='chapterinfo_update'),
+)
