5badf69a51e1cb62491ba3774cdc4c372f10af36
Saloni Shikha <shikhasalonee@gmail.com>
2019-07-25 16:45:24 +0000

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -172,6 +172,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-           'django_filters.rest_framework.DjangoFilterBackend',
+         'django_filters.rest_framework.DjangoFilterBackend',
     ]
 }
